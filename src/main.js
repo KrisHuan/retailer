bd6369d2ef909d1f4a1cfa23eedc00a3c76d6c2e
KrysHuang <1637577696@qq.com>
2022-03-24 00:42:27 +0800

    一秒后挂载

--- src/main.js
+++ src/main.js
@@ -9,7 +9,11 @@ import axios from "axios";
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
 Vue.prototype.$http = axios;
 
-new Vue({
+let vm = new Vue({
   router,
   render: (h) => h(App),
-}).$mount("#app");
+});
+
+setTimeout(() => {
+  vm.$mount("#app");
+}, 1000);
