3467590855b4dfce1940769bb8205dac74b51fc3
KrysHuang <1637577696@qq.com>
2022-03-24 00:02:45 +0800

    Krys完成了登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,22 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../components/Login";
+import Home from "../components/Home";
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes
-})
+  routes: [
+    { path: "/login", component: Login },
+    { path: "/", component: Login },
+    { path: "/home", component: Home },
+  ],
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.path === "/login") return next();
+  const tokenstr = window.sessionStorage.getItem("token");
+  if (!tokenstr) return next("/login");
+  next();
+});
+export default router;
